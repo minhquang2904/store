c9a21662a07716b08523e8ba1a8889b53a368c92
LuongMinhQuang <minhquangnthq@gmail.com>
2024-04-25 17:44:55 +0700

    update

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -521,7 +521,7 @@ const NavBar = () => {
             ref={searchInput}
             type="text"
             placeholder="Search for clothes..."
-            className="text-text xsm:text-[2em] sm:text-[2em]  l:text-[3.4em]  outline-none w-full inputSearch bg-transparent	"
+            className="text-text xsm:text-[2em] sm:text-[2em]  l:text-[3.4em]  outline-none w-full inputSearch bg-primary	"
             contentEditable={true}
           />
           <svg
