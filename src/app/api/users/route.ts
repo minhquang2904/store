7367e787dae446c903311613da88f333241c5b5b
LuongMinhQuang <minhquangnthq@gmail.com>
2024-05-14 22:18:05 +0700

    update

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,14 +3,13 @@ import Users from "@/app/models/Users";
 import { NextResponse } from "next/server";
 
 const GET = async () => {
-  await connectDB();
-
-  try {
-    const users = await Users.find({});
-    return NextResponse.json({ users });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.message });
-  }
+  // await connectDB();
+  // try {
+  //   const users = await Users.find({});
+  //   return NextResponse.json({ users });
+  // } catch (err: any) {
+  //   return NextResponse.json({ error: err.message });
+  // }
 };
 
 export { GET };
