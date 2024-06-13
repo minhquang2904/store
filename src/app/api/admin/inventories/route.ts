758f26244389a2c542f966f48f779aa59f42de6a
LuongMinhQuang <minhquangnthq@gmail.com>
2024-06-13 13:39:08 +0700

    no message

--- src/app/api/admin/inventories/route.ts
+++ src/app/api/admin/inventories/route.ts
@@ -1,16 +1,19 @@
 import connectDB from "@/app/config/connectDB";
 import Inventories from "@/app/models/inventories";
+import Product from "@/app/models/product";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   await connectDB();
   try {
-    const InventoriesCount = await Inventories.find();
-    if (InventoriesCount) {
+    const inventoriesCount = await Inventories.find();
+    const product = await Product.find();
+    if (inventoriesCount) {
       return NextResponse.json({
         message: "Get product Successfully",
         status: 200,
-        data: InventoriesCount,
+        data: inventoriesCount,
+        dataProduct: product,
       });
     }
     return NextResponse.json({
