060412e6206bfb66d31ccbeb47a45709ce66191f
LuongMinhQuang <minhquangnthq@gmail.com>
2024-06-13 11:42:25 +0700

    update

--- src/app/api/admin/inventory/route.ts
+++ src/app/api/admin/inventory/route.ts
@@ -1,17 +1,20 @@
 import connectDB from "@/app/config/connectDB";
+import Categories from "@/app/models/categories";
 import Inventory from "@/app/models/inventory";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
   await connectDB();
 
   try {
     const inventory: any = await Inventory.find();
+    const data: any = await Categories.find();
     if (inventory) {
       return NextResponse.json({
         message: "Get Inventory Successfully",
         status: 200,
         data: inventory,
+        dataCategories: data,
       });
     }
     return NextResponse.json({
