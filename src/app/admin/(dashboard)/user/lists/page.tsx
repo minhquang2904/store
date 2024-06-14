39355a1fc15147377c3980b55d102b4d0b532506
LuongMinhQuang <minhquangnthq@gmail.com>
2024-06-14 19:46:12 +0700

    no message

--- src/app/admin/(dashboard)/user/lists/page.tsx
+++ src/app/admin/(dashboard)/user/lists/page.tsx
@@ -29,7 +29,7 @@ const ListUser = () => {
   const fetchData = () => {
     setLoadingData(true);
     try {
-      fetch(`/api/admin/getUser`)
+      fetch(`/api/admin/getUser?page=${currentPage}&limit=10`)
         .then((res) => res.json())
         .then((data) => {
           console.log("data", data);
