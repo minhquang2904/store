7b7aa3d113d8916c4038c9b441c3a080168ab6df
LuongMinhQuang <minhquangnthq@gmail.com>
2024-10-29 19:19:27 +0700

    no message

--- src/app/(user-with-navigation)/page.tsx
+++ src/app/(user-with-navigation)/page.tsx
@@ -1,34 +1,34 @@
-"use server";
+"use client";
 
-// import dynamic from "next/dynamic";
-// import Banner from "../components/banner/banner";
-// import Brand from "../components/brand/brand";
-// import BannerGroup from "../components/bannerGroup/bannerGroup";
+import dynamic from "next/dynamic";
+import Banner from "../components/banner/banner";
+import Brand from "../components/brand/brand";
+import BannerGroup from "../components/bannerGroup/bannerGroup";
 
-// const TemplateProductView = dynamic(
-//   () => import("../components/template-product-view/templateProductView"),
-//   {
-//     loading: () => <LoadingComponent />,
-//     ssr: true,
-//   }
-// );
-// const TemplateProductSlider = dynamic(
-//   () => import("../components/template-product-slider/templateProductSlider"),
-//   {
-//     loading: () => <LoadingComponent />,
-//     ssr: true,
-//   }
-// );
-// import RelatedProduct from "../components/relatedProduct/relatedProduct";
-// import LoadingComponent from "../components/loadingComponent/loadingComponent";
+const TemplateProductView = dynamic(
+  () => import("../components/template-product-view/templateProductView"),
+  {
+    loading: () => <LoadingComponent />,
+    ssr: true,
+  }
+);
+const TemplateProductSlider = dynamic(
+  () => import("../components/template-product-slider/templateProductSlider"),
+  {
+    loading: () => <LoadingComponent />,
+    ssr: true,
+  }
+);
+import RelatedProduct from "../components/relatedProduct/relatedProduct";
+import LoadingComponent from "../components/loadingComponent/loadingComponent";
 
 const Home = () => {
   return (
     <>
-      {/* <Brand />
+      <Brand />
       <BannerGroup />
       <TemplateProductView />
-      <TemplateProductSlider /> */}
+      <TemplateProductSlider />
     </>
   );
 };
